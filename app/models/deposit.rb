7b234a88681e094fb32d9676f5a78851effa6b22
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-02 04:59:35 +0100

    add association to deposit model

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,3 +1,6 @@
 class Deposit < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :grouped_payments, foreign_key: :deposit_id
+  has_many :groups, through: :grouped_payments
+
 end
