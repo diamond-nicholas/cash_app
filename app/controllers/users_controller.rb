ade0d8d31d564adbff60077f1bf5e9c82e5aa998
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-03 15:49:29 +0100

    add some method to the user.rb model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user[:id])
   end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
    
   has_many :authored_deposits, foreign_key: :author_id, class_name: 'Deposit', dependent: :destroy
   has_many :groups, dependent: :destroy
+
+
+    def grouped
+      authored_deposits.joins(:grouped_payments)
+        .order('name DESC')
+        .where('grouped_payments.group_id >= 1')
+        .group('deposits.id')
+    end
+
+  def ungrouped
+    authored_deposits.includes(:grouped_payments).order('name DESC').map do |deposit|
+      deposit if deposit.grouped_payments.empty?
+    end.compact
+  end
 end
