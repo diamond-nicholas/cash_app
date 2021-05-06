b44658c452e445626545653847c2fba357785ab6
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-06 06:55:10 +0100

    update the group controller through current_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,11 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
+  
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups.order('name ASC')
+    @total = @groups.count(:name)
   end
 
   # GET /groups/1 or /groups/1.json
@@ -21,7 +23,7 @@ class GroupsController < ApplicationController
 
   # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
 
     respond_to do |format|
       if @group.save
