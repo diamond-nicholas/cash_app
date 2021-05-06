70710ab5d970bd3e7067b52bb279b2c6ef45aca1
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-06 07:23:40 +0100

    add validation to group model

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -3,6 +3,7 @@ class Deposit < ApplicationRecord
   has_many :grouped_payments, foreign_key: :deposit_id
   has_many :groups, through: :grouped_payments
 
+  
 
    def grouped_payment_find(group)
     grouped_payments.find_by(group_id: group.id)
