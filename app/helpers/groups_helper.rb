f102ff4b4ad127de9a59fbac9611dd3a3c583de9
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-07 03:31:08 +0100

    update the group views

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def views_show_deposits(group)
+    out = ''
+    group.deposits.includes(:author).each do |deposit|
+      out += render 'deposit_list', deposit: deposit
+    end
+    out.html_safe
+  end
 end
