b22af6233da571de2a56ab2667f8e6ae6ec7321a
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-08 20:39:02 +0100

    fix linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validation tests' do
     it 'ensures name presence' do
-      user = User.new(email: 'diamond@gmail.com', password: 123456789).save
+      user = User.new(email: 'diamond@gmail.com', password: 123_456_789).save
       expect(user).to eq(false)
     end
 
     it 'ensures email presence' do
-      user = User.new(name: 'nicholas', password: 123456789).save
+      user = User.new(name: 'nicholas', password: 123_456_789).save
       expect(user).to eq(false)
     end
 
     it 'should save successfully' do
-      user = User.new(name: 'nicholas', email: 'diamond@gmail.com', password: 123456789).save
+      user = User.new(name: 'nicholas', email: 'diamond@gmail.com', password: 123_456_789).save
       expect(user).to eq(true)
     end
   end
