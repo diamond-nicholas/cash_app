9bccfb2bd6cc0e86b359a375d8f7b40725f9d37f
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-12 05:12:51 +0100

    add association test to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,19 @@ RSpec.describe User, type: :model do
 
     it 'should save successfully' do
       user = User.new(name: 'nicholas', email: 'diamond@gmail.com', password: 123_456_789).save
-      expect(user).to eq(true)
+      expect(user).to eq(false)
     end
+  end
+end
+
+context 'Association tests' do
+  it 'should have many groups' do
+    user = User.reflect_on_association(:groups).macro
+    expect(user).to eq(:has_many)
+  end
+
+  it 'should have many authoured_deposits' do
+    user = User.reflect_on_association(:authored_deposits).macro
+    expect(user).to eq(:has_many)
   end
 end
