ec9ff4c66a33416390b2a6f7ce3c40c40e73b6a7
diamond-nicholas <diamondnicholas154@gmail.com>
2021-05-12 05:58:03 +0100

    add more feature test to user

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -23,4 +23,34 @@ RSpec.feature 'Users', type: :feature do
       expect(current_path).not_to eq('/')
     end
   end
+
+  context 'user login' do
+    scenario 'it should succesfully login' do
+      visit new_user_session_path
+        fill_in 'Email', with: 'diamond@gmail.com'
+        fill_in 'Password', with: 123456789
+      click_on 'Log in'
+      expect(current_path).to eq('/')
+    end
+
+    scenario 'it should not fail to login' do
+      visit new_user_session_path
+      fill_in 'Email', with: 'diamond'
+      fill_in 'Password', with: 123456789
+      click_on 'Log in'
+      expect(current_path).to eq('/users/sign_in')
+    end
+  end
+  
+  context 'log out' do
+    scenario 'Log Out' do
+      visit new_user_session_path
+      fill_in 'Email', with:'diamond@gmail.com'
+      fill_in 'Password', with: 123456789
+      click_on 'Log in'
+      visit root_path
+      expect(page).to have_content('Log Out')
+    end
+  end
+
 end
